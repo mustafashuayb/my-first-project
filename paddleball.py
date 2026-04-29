51f394a58e289ef5496259ad6f632abffe68be7f
Mustafa Shuayb <mustafashuayb1@gmail.com>
2026-04-29 12:40:34 +0300

    added game loop

--- paddleball.py
+++ paddleball.py
@@ -16,4 +16,11 @@ tk.wm_attributes('-topmost', 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0) 
 canvas.pack() 
 tk.update()
+
 ball = Ball(canvas, 'red')
+
+while True: 
+    ball.draw()
+    tk.update_idletasks() 
+    tk.update() 
+    time.sleep(0.01)
