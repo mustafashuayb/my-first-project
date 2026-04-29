5ec941b8f910508ff93eecce26a26d6282c26520
Mustafa Shuayb <mustafashuayb1@gmail.com>
2026-04-29 13:23:24 +0300

    heavily changed ball class and draw function

--- paddleball.py
+++ paddleball.py
@@ -10,19 +10,27 @@ class Ball:
         self.canvas = canvas 
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color) 
         self.canvas.move(self.id, 245, 100)
-        # Making The Ball Bounce
-        self.x = 0 
-        self.y = -1 
+        # Randomly Choosing the Starting X Direction of the Ball
+        starts = [-3, -2, -1, 1, 2, 3] 
+        self.x = random.choice(starts)
+        self.y = -3 
         self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
         
-    # Moving/Bouncing the Ball if it Hits the Top or Bottom of the Canvas     
-    def draw(self): 
+    def draw(self):
+        # Moving the Ball in the X and Y Direction 
         self.canvas.move(self.id, self.x, self.y) 
-        pos = self.canvas.coords(self.id) 
+        pos = self.canvas.coords(self.id)
+
+        # Bouncing the Ball if it Hits the Top or Bottom of the Canvas
         if pos[1] <= 0: 
-            self.y = 1  
+            self.y = 1      
         if pos[3] >= self.canvas_height: 
             self.y = -1
+            
+        # Bouncing the Ball if it Hits the Left or Right of the Canvas    
+        if pos[0] <= 0 or pos[2] >= self.canvas_width: 
+            self.x = self.x * -1
 
 
 # Tkinter Window 
