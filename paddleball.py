f6f20a97dcaf9d03d4386b9026b756f7abd216c0
Mustafa Shuayb <mustafashuayb1@gmail.com>
2026-04-29 12:45:59 +0300

    changed draw function

--- paddleball.py
+++ paddleball.py
@@ -1,13 +1,15 @@
 from tkinter import * 
 import random 
 import time
+
 class Ball: 
     def __init__(self, canvas, color): 
         self.canvas = canvas 
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color) 
         self.canvas.move(self.id, 245, 100) 
+ 
     def draw(self): 
-        pass
+        self.canvas.move(self.id, 0, -1)
 
 tk = Tk() 
 tk.title('Bounce Game') 
