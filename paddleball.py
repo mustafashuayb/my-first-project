c5ec8bc26c0a19fe1cfa2b32d4bbef92305c7fe4
Mustafa Shuayb <mustafashuayb1@gmail.com>
2026-04-29 13:00:19 +0300

    Added Comments

--- paddleball.py
+++ paddleball.py
@@ -1,28 +1,35 @@
+# Libaries
 from tkinter import * 
 import random 
-import time
-
+import time 
+ 
+ # Ball Class
 class Ball: 
+    # Creating Canvas, Color and Ball
     def __init__(self, canvas, color): 
         self.canvas = canvas 
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color) 
         self.canvas.move(self.id, 245, 100) 
-
+        
+    # Moving the Ball     
     def draw(self): 
-        self.canvas.move(self.id, 0, -1)
+        self.canvas.move(self.id, 0, -1) 
 
+# Tkinter Window 
 tk = Tk() 
 tk.title('Bounce Game') 
 tk.resizable(0, 0) 
 tk.wm_attributes('-topmost', 1) 
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0) 
 canvas.pack() 
-tk.update()
+tk.update() 
 
-ball = Ball(canvas, 'red')
+# Creating the Ball 
+ball = Ball(canvas, 'red') 
 
+# Main game loop
 while True: 
-    ball.draw()
+    ball.draw() 
     tk.update_idletasks() 
     tk.update() 
     time.sleep(0.01)
