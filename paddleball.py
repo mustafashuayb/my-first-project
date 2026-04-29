455bb0a38f7d5dcb87050cd9c6a82da21dcf3f77
Mustafa Shuayb <mustafashuayb1@gmail.com>
2026-04-29 13:05:58 +0300

    Added Bouncing on canvas, changed ball class on draw function

--- paddleball.py
+++ paddleball.py
@@ -9,11 +9,21 @@ class Ball:
     def __init__(self, canvas, color): 
         self.canvas = canvas 
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color) 
-        self.canvas.move(self.id, 245, 100) 
+        self.canvas.move(self.id, 245, 100)
+        # Making The Ball Bounce
+        self.x = 0 
+        self.y = -1 
+        self.canvas_height = self.canvas.winfo_height()
         
-    # Moving the Ball     
+    # Moving/Bouncing the Ball if it Hits the Top or Bottom of the Canvas     
     def draw(self): 
-        self.canvas.move(self.id, 0, -1) 
+        self.canvas.move(self.id, self.x, self.y) 
+        pos = self.canvas.coords(self.id) 
+        if pos[1] <= 0: 
+            self.y = 1  
+        if pos[3] >= self.canvas_height: 
+            self.y = -1
+
 
 # Tkinter Window 
 tk = Tk() 
